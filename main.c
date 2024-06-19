dfbe8ca5542014e0d80d8676d528fd1ccf072645
Natália Medeiros Da Silva De Almeida <natalia@Natalias-MBP.home>
2024-06-19 14:30:42 +0200

    update cmd line checker

--- main.c
+++ main.c
@@ -6,47 +6,35 @@
 /*   By: natalia <natalia@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/05/28 11:41:54 by natalia       #+#    #+#                 */
-/*   Updated: 2024/06/19 14:08:38 by natalia       ########   odam.nl         */
+/*   Updated: 2024/06/19 14:29:02 by natalia       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-bool	is_cmd_line_calid(t_data *data)
+bool	is_cmd_line_valid(char *cmd)
 {
 	int		i;
-	char	cmd;
 
 	i = 0;
-	while (data->command_line[i] != '\0')
+	while (cmd[i] != '\0')
 	{
-		if (data->command_line[i + 1] == '\0' && (data->command_line[i] == '|'
-				|| data->command_line[i] == '<' || data->command_line[i] == '>'))
-			return (ft_putendl_fd(2, "Syntax Error"), false);
-		cmd = data->command_line[i];
-		if (cmd == '|' || cmd =='>' || cmd == '<')
+		if(cmd[i] == '|' || cmd[i] =='>' || cmd[i] == '<')
 		{
+			if(cmd[i + 1] == '\0')
+				return (ft_putendl_fd(2, "Syntax Error"), false);
 			i++;
-			while (data->command_line[i] == ' ' && data->command_line[i + 1] != '\0')
+			if (cmd[i] == '|')
+				return (ft_putendl_fd(2, "Syntax Error"), false);
+			else if (cmd[i] =='>' || cmd[i] == '<')
 				i++;
-			if (data->command_line[i + 1] == '\0')
+			while (cmd[i] == ' ' && cmd[i + 1] != '\0')
+				i++;
+			if (cmd[i + 1] == '\0')
+				return (ft_putendl_fd(2, "Syntax Error"), false);
+			if (cmd[i] == '|' || cmd[i] =='>' || cmd[i] == '<')
 				return (ft_putendl_fd(2, "Syntax Error"), false);
 		}
-		if (data->command_line[i] == '<' || data->command_line[i] == '>')
-		{
-			if (data->command_line[i + 1] == '|' || data->command_line[i + 2] == '|'
-				|| data->command_line[i + 2] == '<' || data->command_line[i + 2] == '>') // only take care of one space!
-				return (ft_putendl_fd(2, "Syntax Error"), false);
-		}
-		if (data->command_line[i] == '|')
-		{
-			if ((data->command_line[i + 1] == '|' || data->command_line[i + 1] == '<'
-				|| data->command_line[i + 1] == '>') || (data->command_line[i + 2] == '|'
-				|| data->command_line[i + 2] == '<' || data->command_line[i + 2] == '>'))
-				return (ft_putendl_fd(2, "Syntax Error"), false);
-		}
-		// if (prompt[i] == ';' || prompt[i] == '\\')
-		// 	return (ft_putendl_fd(2, "Syntax Error"), false);
 		i++;
 	}
 	return (true);
@@ -54,24 +42,24 @@ bool	is_cmd_line_calid(t_data *data)
 
 int	main(int argc, char **argv, char **envp)
 {
-	t_data	*data;
+	t_data	data;
 
 	if (argc != 1 && argv)
 		printf("too much argments\n");
-	data = malloc(sizeof(t_data) * 1);
-	if (data == NULL)
-		return (EXIT_FAILURE);
+	// data = malloc(sizeof(t_data) * 1);
+	// if (data == NULL)
+	// 	return (EXIT_FAILURE);
 	while (1)
 	{
-		data->command_line = readline("[minishell]: ");
-		if (is_cmd_line_calid(data) == false)
+		data.command_line = readline("[minishell]: ");
+		if (is_cmd_line_valid(data.command_line) == false)
 			return (1);
-		printf("---%s---\n", data->command_line);
-		printf("---ls > \n---\n");
-		data->envp = envp;
-		add_history(data->command_line);
-		parser(&data);
-		check_path(data->command_line, data->envp);
+		// printf("---%s---\n", data->command_line);
+		// printf("---ls > \n---\n")ls > ;
+		data.envp = envp;
+		add_history(data.command_line);
+		// parser(&data);
+		// check_path(data->command_line, data->envp);
 		rl_on_new_line();
 	}
 	return (0);
