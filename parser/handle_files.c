ce06a79d3a3078547adfe3b822f10cd241310294
Natália Medeiros Da Silva De Almeida <natalia@Natalias-MBP.home>
2024-08-30 14:23:11 +0200

    changed function name, create fill temp function, added free_array temp when it is necessary

--- parser/handle_files.c
+++ parser/handle_files.c
@@ -6,19 +6,36 @@
 /*   By: natalia <natalia@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/07/23 15:10:34 by natalia       #+#    #+#                 */
-/*   Updated: 2024/08/30 13:33:59 by natalia       ########   odam.nl         */
+/*   Updated: 2024/08/30 14:22:14 by natalia       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	ft_bnull(char **temp, int nb)
+void	ft_be_null(char **temp, int nb)
 {
 	nb--;
 	while (nb >= 0)
 	{
 		temp[nb] = NULL;
 		nb--;
+	}
+}
+void	fill_temp(char **temp, int i, char *cmd, int j)
+{
+	if (j == 0)
+	{
+		temp[0] = ft_calloc((i + 1), sizeof(char));
+		if (temp[0] == NULL)
+			return ;
+		ft_strlcpy(temp[0], cmd, i + 1);
+	}
+	else
+	{
+		temp[1] = ft_calloc((i + 1), sizeof(char));
+		if (temp[1] == NULL)
+			return ;
+		ft_strlcpy(temp[1], cmd + i, (j - i + 1));
 	}
 }
 char **split_redirection_first(char *cmd)
@@ -28,41 +45,34 @@ char **split_redirection_first(char *cmd)
 	int		j;
 
 	i = 0;
-	temp = malloc(3 * sizeof(char *));
+	j = 0;
+	temp = ft_calloc(3, sizeof(char *));
 	if (temp == NULL)
 		return (NULL);
-	ft_bnull(temp, 3);
+	ft_be_null(temp, 3);
 	while (cmd[i] != ' ' && cmd[i] != '\0')
 		i++;
-	temp[0] = ft_calloc((i + 1), sizeof(char));
-	if (temp[0] == NULL)
-		return (NULL);
-	ft_strlcpy(temp[0], cmd, i + 1);
+	fill_temp(temp, i, cmd, j);
 	i++;
 	j = i;
 	while (cmd[j] != '\0')
 		j++;
 	if (j > i)
-	{
-		temp[1] = ft_calloc((i + 1), sizeof(char));
-		if (temp[1] == NULL)
-			return (NULL);
-		ft_strlcpy(temp[1], cmd + i, (j - i + 1));
-	}
+		fill_temp(temp, i, cmd, j);
+	if (temp[0] == NULL || temp[1] == NULL)
+		return (free_array(3, temp), NULL);
 	return(temp);
 }
 
 int	handle_files(t_data data, t_parser **parser, int i, bool has_pipe)
 {
 	bool	start_with_redirection;
-	// printf("*%c\n", data.cmd_table[i][0]);
 	if (i == 0 || has_pipe == true)
 		start_with_redirection = true;
 	else
 		start_with_redirection = false;
 	if (data.cmd_table[i][0] == '>')
 	{
-		// printf("Entrei no if\n");
 		if (handle_outfile(data, parser, i, start_with_redirection) != 0)
 			return (error_msg("failurre on handle outfile"), 1);
 	}
